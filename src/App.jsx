fa131ed9b121cf176954ef8f3f806a87e0a15330
Anne Hunt <acathunt@gmail.com>
2024-09-01 15:41:05 -0600

    fixed vite error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
@@ -7,6 +7,21 @@ import Modal from './components/modal.jsx'
 
 function App() {
   const [showModal, setShowModal] = useState(false)
+
+  useEffect(() => {
+    async function getAPODData() {
+      const APOD_KEY = import.meta.env.VITE_API_KEY
+      const apiURL = 'https://api.nasa.gov/planetary/apod' + `?api_key=${APOD_KEY}`
+      try {
+        const response = await fetch(apiURL)
+        const data = await response.json()
+        console.log("Success", data)
+      } catch (error) {
+        console.log("Unable to get APOD", error.message)
+      }
+    }
+    getAPODData()
+  }, [])
 
   function toggleModal() {
     setShowModal(!showModal)
